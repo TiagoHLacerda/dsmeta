1ee7162d2b52eeba1f317b62aba1f1277734c122
TiagoHLacerda <51682610+TiagoHLacerda@users.noreply.github.com>
2022-07-13 21:00:57 -0300

    Project dsmeta

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard/sales"
+
+function App() {
+  return (
+    <>
+
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+
+            https://github.com/devsuperior/sds-dsmeta/tree/main/episodio1
+          </div>
+        </section>
+      </main>
+
+
+
+
+
+
+
+
+
+
+
+    </>
+
+
+
+  )
+}
+
+export default App
